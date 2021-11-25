49fa468498adbd4f56be5f2d314ed74f18835aa8
Boumi <saidiboumediene98@gmail.com>
2021-11-25 10:26:27 +0100

    handling some changes

--- src/MainDemo.js
+++ src/MainDemo.js
@@ -1,22 +1,73 @@
 import Stat from "./Stat";
 import useFetch from "./utils/useFetch";
-import { useState } from "react";
+import { useState /*useEffect*/ } from "react";
 import Map from "./Map";
+import Spinner from "react-bootstrap/Spinner";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import DataTable from "react-data-table-component";
+//const axios = require("axios").default;
 const MainDemo = ({ query, runQuery, result, setResult, nodeUrl }) => {
+  /************************* Data Table  ******************************/
+  /*const [resultData, setResultData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalRows, setTotalRows] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+
+  const fetchUsers = async (page) => {
+    setLoading(true);
+
+    const response = await axios.get(
+      `https://reqres.in/api/users?page=${page}&per_page=${perPage}&delay=1`
+    );
+
+    setResultData(response.data.data);
+    setTotalRows(response.data.total);
+    setLoading(false);
+  };
+
+  const handlePageChange = (page) => {
+    fetchUsers(page);
+  };
+
+  const handlePerRowsChange = async (newPerPage, page) => {
+    setLoading(true);
+
+    const response = await axios.get(
+      `http://localhost:3005/fetchData?page=${page}&per_page=${newPerPage}&delay=1`
+    );
+
+    setResultData(response.data.data);
+    setPerPage(newPerPage);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUsers(1); // fetch page 1 of users
+  }, []);*/
+  /********************************************************************** */
+
   const [isSpa, setIsSpa] = useState(false);
   const [showRow, setShowRow] = useState(false);
   const [resultType, setResultType] = useState(1);
   const [showResult, setShowResult] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   /** fetch data queries return {"queriesWithResults":[]} */
   const { data, setData } = useFetch("/demo");
   /** Run Query: update the existing queries after getting the result*/
   const hundleRunQuery = () => {
     Promise.all([runQuery()]).then(([newData]) => {
+      console.log(newData);
+      if (Object.keys(newData["currentQuery"]).length === 0) {
+        alert("OOOOh lala sorry we don't have this db or this query");
+        setIsLoading(false);
+        return;
+      }
       setData(newData);
       setResult(newData["currentQuery"]["result"]);
       setResultType(1);
       setIsSpa(newData["currentQuery"]["isSpatial"] === "true");
       if (showRow === false) setShowRow(true);
+      setIsLoading(false);
       /*if(Object.keys(newQueryWithResult).length !== 0){
                 let newArr = [...data["queriesWithResults"]];
                 newArr[data["queriesWithResults"].length] = newQueryWithResult;
@@ -58,7 +109,10 @@ const MainDemo = ({ query, runQuery, result, setResult, nodeUrl }) => {
             <button
               className="btn btn-sm btn-outline-secondary"
               id="run_btn"
-              onClick={() => hundleRunQuery()}
+              onClick={() => {
+                setIsLoading(true);
+                hundleRunQuery();
+              }}
             >
               Run
             </button>
@@ -114,8 +168,45 @@ const MainDemo = ({ query, runQuery, result, setResult, nodeUrl }) => {
           </div>
         </div>
       </div>
-      {resultSection}
-
+      {/*resultSection*/}
+      {/*<DataTable
+        title="Users"
+        columns={[
+          {
+            name: "First Name",
+            selector: (row) => row.first_name,
+          },
+          {
+            name: "Last Name",
+            selector: (row) => row.last_name,
+          },
+          {
+            name: "Email",
+            selector: (row) => row.email,
+          },
+        ]}
+        data={resultData}
+        progressPending={loading}
+        pagination
+        paginationServer
+        paginationTotalRows={totalRows}
+        onChangeRowsPerPage={handlePerRowsChange}
+        onChangePage={handlePageChange}
+      />*/}
+      <div className="d-flex justify-content-center">
+        {isLoading ? (
+          <Spinner
+            animation="border"
+            role="status"
+            size="md"
+            style={{ width: "10rem", height: "10rem" }}
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        ) : (
+          resultSection
+        )}
+      </div>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
         <h1 className="h2">Stats</h1>
       </div>
