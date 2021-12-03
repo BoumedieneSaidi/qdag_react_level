72fd714c55e6ad28275037e59a49303dd8efba2f
Boumi <saidiboumediene98@gmail.com>
2021-12-03 12:06:25 +0100

    adding base name 'sub derectory'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import Navbar from './Navbar';
-import Home from './static_pages/Home';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
-import Contact from './static_pages/Contact';
-import PublicationList from './static_pages/PublicationList';
-import Demo from './Demo';
+import Navbar from "./Navbar";
+import Home from "./static_pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contact from "./static_pages/Contact";
+import PublicationList from "./static_pages/PublicationList";
+import Demo from "./Demo";
 function App() {
   return (
-    <Router>
-          <Navbar/>
-          <Switch>
-              <Route exact path="/">
-                  <Home />
-              </Route>
-              <Route path="/demo">
-                  <Demo />
-              </Route>
-              <Route path="/contact">
-                  <Contact />
-              </Route>
-              <Route path="/publications">
-                  <PublicationList />
-              </Route>
-          </Switch>
+    <Router basename={"/qdag"}>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/demo">
+          <Demo />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/publications">
+          <PublicationList />
+        </Route>
+      </Switch>
     </Router>
   );
 }
